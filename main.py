de231c3df959c2cda1fdda739f396d9818e7a7de
Kurumii <69804977+Kurumii-ii@users.noreply.github.com>
2024-03-16 16:12:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ def countdown(t, msg):
     while t:
         secs = divmod(t, 60)
         timer = f"{secs[1]}"
-        print(f"{msg} {timer} Seconds", end="\r")
+        print(
+            f"[ + ] Changed {msg} Successfully, Redirecting To Main Menu In {timer} Seconds",
+            end="\r",
+        )
         time.sleep(1)
         t -= 1
 
@@ -31,6 +34,13 @@ def image_path():
     clear()
     os.system("ls")
     avatar_path = input("\n[ + ] Choose From Above or Drag The Image Here ~> ")
+
+    if "'" in avatar_path:
+        avatar_path = avatar_path.replace("'", "")
+
+    if '"' in avatar_path:
+        avatar_path = avatar_path.replace('"', "")
+
     try:
         with open(avatar_path, "rb") as file:
             image_content = file.read()
@@ -51,56 +61,57 @@ def patch_api(choice, file_path, endpoint):
 
 
 def main():
-    verify = verify_token(token)
-    if verify.status_code == 200:
-        while True:
-            clear()
-            option = input(
-                "Bot Username : "
-                + verify.json()["username"]
-                + "#"
-                + verify.json()["discriminator"]
-                + "\n\n[ 1 ] Change Bot Avatar\n[ 2 ] Change Bot Banner\n[ 3 ] Change Application Icon\n[ 4 ] Exit\n[ ~ ] Enter The Number Of Desired Option Here ~> "
-            )
-            if option == "1":
-                if patch_api("avatar", image_path(), "users").status_code == 200:
-                    countdown(
-                        5,
-                        "[ + ] Changed Bot Avatar Successfully, Redirecting To Main Menu In",
-                    )
-                    clear()
-                else:
-                    print("[ ! ] Failed To Change Bot Avatar")
-                    break
-            elif option == "2":
-                if patch_api("banner", image_path(), "users").status_code == 200:
-                    countdown(
-                        5,
-                        "[ + ] Changed Bot Banner Successfully, Redirecting To Main Menu In",
-                    )
-                    clear()
-                else:
-                    print("[ ! ] Failed To Change Bot Banner")
-                    break
-            elif option == "3":
-                if patch_api("icon", image_path(), "applications").status_code == 200:
-                    countdown(
-                        5,
-                        "[ + ] Changed Application Icon Successfully, Redirecting To Main Menu In",
-                    )
-                    clear()
-                else:
-                    print("[ ! ] Failed To Change Application Icon")
-            elif option == "4":
-                print("[ ! ] Exiting...")
-                break
-            else:
-                print("[ ! ] Invalid Input")
-                time.sleep(3)
+    while True:
+        clear()
+        option = input(
+            "Bot Username : "
+            + verify.json()["username"]
+            + "#"
+            + verify.json()["discriminator"]
+            + "\n\n[ 1 ] Change Bot Avatar\n[ 2 ] Change Bot Banner\n[ 3 ] Change Application Icon\n[ 4 ] Exit\n[ ~ ] Enter The Number Of Desired Option Here ~> "
+        )
+        if option == "1":
+            if patch_api("avatar", image_path(), "users").status_code == 200:
+                countdown(
+                    5,
+                    "Bot Avatar",
+                )
                 clear()
-                pass
-    else:
-        print("[ ! ] Invalid Token")
+            else:
+                print("[ ! ] Failed To Change Bot Avatar")
+                break
+        elif option == "2":
+            if patch_api("banner", image_path(), "users").status_code == 200:
+                countdown(
+                    5,
+                    "Bot Banner",
+                )
+                clear()
+            else:
+                print("[ ! ] Failed To Change Bot Banner")
+                break
+        elif option == "3":
+            if patch_api("icon", image_path(), "applications").status_code == 200:
+                countdown(
+                    5,
+                    "Application Icon",
+                )
+                clear()
+            else:
+                print("[ ! ] Failed To Change Application Icon")
+                break
+        elif option == "4":
+            print("[ ! ] Exiting...")
+            break
+        else:
+            print("[ ! ] Invalid Input")
+            time.sleep(3)
+            clear()
+            pass
 
 
-main()
+verify = verify_token(token)
+if verify.status_code == 200:
+    main()
+else:
+    print("[ ! ] Invalid Token")
